a763f0f879a6aca7a8a4d02f83c2736e98a513a5
J.K <januprasad@JKs-MacBook-Pro.local>
2022-08-26 11:43:25 +0530

    List items to UI

--- app/src/main/java/com/github/tweeny/ui/MainApp.kt
+++ app/src/main/java/com/github/tweeny/ui/MainApp.kt
@@ -1,16 +1,23 @@
 package com.github.tweeny.ui
 
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.github.tweeny.model.Post
 import com.github.tweeny.uiState.ApiState
 import com.github.tweeny.viewmodel.MainViewModel
 
@@ -18,27 +25,37 @@ import com.github.tweeny.viewmodel.MainViewModel
 fun MainApp() {
     val viewModel = viewModel(modelClass = MainViewModel::class.java)
     val state by viewModel._postStateFlow.collectAsState()
-    LazyColumn {
+    Surface(modifier = Modifier.fillMaxSize()) {
         when (state) {
             is ApiState.Failure -> {
-                item { Text(text = "API Error") }
+                Text(text = "API Error")
             }
             is ApiState.Loading -> {
-                item {
-                    CircularProgressIndicator(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .wrapContentSize(align = Alignment.Center)
-                    )
-                }
+                CircularProgressIndicator(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .wrapContentSize(align = Alignment.Center)
+                )
             }
             is ApiState.Success -> {
-                item {
-                    Text(text = "Success", modifier = Modifier.fillMaxSize().wrapContentSize(
-                        Alignment.Center))
-                }
+                PostList((state as ApiState.Success).data)
             }
             is ApiState.Empty -> {
+            }
+        }
+    }
+}
+
+@Composable
+fun PostList(data: List<Post>) {
+    Card(modifier = Modifier.fillMaxSize()) {
+        Text(text = "Posts")
+        LazyColumn(
+            modifier = Modifier.fillMaxWidth(),
+            contentPadding = PaddingValues(16.dp)
+        ) {
+            items(data) { item ->
+                Text(text = item.body)
             }
         }
     }
